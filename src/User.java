18ec261ee8b5e455de1c484dc0de0246d328fa2a
universestu <universestu@gmail.com>
2016-11-15 14:33:27 +0700

    edit null input and throw exception

--- src/User.java
+++ src/User.java
@@ -1,6 +1,6 @@
 public class User implements IUser {
-    private String name;
-    private String password;
+    private String name ="Admin";
+    private String password = "admin";
     public int type;
 
     @Override
@@ -10,19 +10,22 @@ public class User implements IUser {
     }
 
     @Override
-    public String setName(String name)
-    {if(name.length() != 0)
-        return null;
+    public String setName(String name) throws RuntimeException
+    {if(name.length() == 0)
+       throw new RuntimeException("Name is null,Please check your name again");
     else{
         this.name = name;
         return name;}
     }
 
     @Override
-    public String setPassword(String password) {
+    public String setPassword(String password) throws RuntimeException{
 
-    if(password.length() != 0)
-        return null;
+    if(password.length() == 0){
+        throw new RuntimeException("password is null,Please check your password again");}
+    if(password.length() < 4) {
+            throw new RuntimeException("password is more 4 character,Please add more character again");
+        }
      else
     {this.password = password;
         return password;}
