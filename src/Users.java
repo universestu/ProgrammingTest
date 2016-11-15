0572f55826eef1c4b011611ea09b9560b21eb805
satiando5826 <sathianpornkhamdee@gmail.com>
2016-11-15 14:35:37 +0700

    finish

--- src/Users.java
+++ src/Users.java
@@ -7,32 +7,57 @@ public class Users {
     // Create new user with type, name and password
     // user created with this method should be automatically added to userList;
     public IUser create(int type, String name, String password) {
-        return null;
+        if(type == 1){
+            Student student = new Student();
+            student.setName(name);
+            student.setPassword(password);
+            student.setType();
+            userList.add(student);
+        }else{
+            Teacher teacher = new Teacher();
+            teacher.setName(name);
+            teacher.setPassword(password);
+            teacher.setType();
+            userList.add(teacher);
+        }
+        return userList.get(0);   //return first index of ArrayList ? = 0 ??
     }
 
     // Add new user to repository
-    public void add(IUser user) {
-
+    public void add(IUser user) throws RuntimeException {
+        if(userList.contains(user)) {
+              throw new RuntimeException("This user was used");
+        }
+        userList.add(user); //only add a user ?
     }
 
     // Delete user from repository
     // Throw  RuntimeException if the user is not in the list
     public void remove(IUser user) throws RuntimeException {
-
+        if(!userList.contains(user)){                       //user is not in the list
+            throw new RuntimeException("user is not in the list");
+        }
+        userList.remove(user);
     }
 
     // Return true if the user is in the list
     public boolean exists(IUser user) {
-        return false;
+        return userList.contains(user);
     }
 
     // Return number of user in the list
     public int count() {
-        return 0;
+        return userList.size();
     }
 
     // Return number of user in the list, according to type
     public int countByType(int type) {
-        return 0;
+        int count=0;
+        for(int i=0;i<this.count();i++){
+            if(userList.get(i).getType()==type){
+                count++;
+            }
+        }
+        return count;
     }
 }
